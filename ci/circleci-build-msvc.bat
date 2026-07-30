e03c244e49820e8ad25f24a91e9c8820511d3b33
Paul OBrien <leblanc_leblanc@hotmail.com>
2026-07-30 23:10:10 +0100

    Run native Windows and macOS validation

--- ci/circleci-build-msvc.bat
+++ ci/circleci-build-msvc.bat
@@ -2,6 +2,9 @@
 :: Build the msvc artifacts
 ::
 
+@echo on
+setlocal EnableExtensions
+
 set "SCRIPTDIR=%~dp0"
 set "GIT_HOME=C:\Program Files\Git"
 if "%CONFIGURATION%" == "" set "CONFIGURATION=RelWithDebInfo"
@@ -16,8 +19,10 @@ dir %SCRIPTDIR%
 dir %SCRIPTDIR%..
 dir %SCRIPTDIR%..\msvc
 call %SCRIPTDIR%..\msvc\win_deps.bat %wx_vers%
+if errorlevel 1 exit /b %errorlevel%
 path %SCRIPTDIR%.local\bin;%PATH%;"C:\Program Files\CMake\bin"
 call "%SCRIPTDIR%..\cache\wx-config.bat"
+if errorlevel 1 exit /b %errorlevel%
 set PATH=%EXTRA_PATH%;%PATH%
 echo EXTRA_PATH: %EXTRA_PATH%
 echo USING wxWidgets_LIB_DIR: %wxWidgets_LIB_DIR%
@@ -31,6 +36,7 @@ if errorlevel 1 (
 )
 
 git submodule update --init opencpn-libs
+if errorlevel 1 exit /b %errorlevel%
 
 dir
 
@@ -39,10 +45,15 @@ mkdir build && cd build
 dir
 
 wget https://sourceforge.net/projects/opencpnplugins/files/opencpn.lib
+if errorlevel 1 exit /b %errorlevel%
 wget https://download.opencpn.org/s/oibxM3kzfzKcSc3/download/OpenCPN_buildwin-4.99a.7z
+if errorlevel 1 exit /b %errorlevel%
 7z x -y OpenCPN_buildwin-4.99a.7z -o..\buildwin
+if errorlevel 1 exit /b %errorlevel%
 wget https://download.opencpn.org/s/54HsBDLNzRZLL6i/download/nsis-3.04-setup.exe
+if errorlevel 1 exit /b %errorlevel%
 nsis-3.04-setup.exe /S
+if errorlevel 1 exit /b %errorlevel%
 
 echo Check if poedit has been installed
 poedit -version
@@ -55,7 +66,7 @@ cmake -T v141_xp -G "Visual Studio 16 2019" ^
     -DCMAKE_GENERATOR_PLATFORM=Win32 ^
     -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
     -DXWEATHER_ROUTING_STANDALONE_API=ON ^
-    -DOCPN_BUILD_TEST=OFF ^
+    -DOCPN_BUILD_TEST=ON ^
     -DwxWidgets_LIB_DIR=%wxWidgets_LIB_DIR% ^
     -DwxWidgets_ROOT_DIR=%wxWidgets_ROOT_DIR% ^
     ..
@@ -64,11 +75,12 @@ cmake -A Win32 -G "Visual Studio 17 2022" ^
     -DCMAKE_GENERATOR_PLATFORM=Win32 ^
     -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
     -DXWEATHER_ROUTING_STANDALONE_API=ON ^
-    -DOCPN_BUILD_TEST=OFF ^
+    -DOCPN_BUILD_TEST=ON ^
     -DwxWidgets_LIB_DIR=%wxWidgets_LIB_DIR% ^
     -DwxWidgets_ROOT_DIR=%wxWidgets_ROOT_DIR% ^
     ..
 )
+if errorlevel 1 exit /b %errorlevel%
 
 
 cd
@@ -76,5 +88,48 @@ dir
 
 echo Build for windows
 
-:cmake --build . --target tarball --config %CONFIGURATION%
-cmake --build . --target package --config %CONFIGURATION%
+cmake --build . --config %CONFIGURATION% --parallel 2
+if errorlevel 1 exit /b %errorlevel%
+
+if not exist test-results mkdir test-results
+ctest --test-dir . -C %CONFIGURATION% --output-on-failure --timeout 180 --output-junit test-results\ctest.xml
+if errorlevel 1 exit /b %errorlevel%
+
+if exist stage rmdir /s /q stage
+cmake --install . --config %CONFIGURATION% --prefix stage
+if errorlevel 1 exit /b %errorlevel%
+if not exist stage\plugins\xweather_routing_pi.dll (
+  echo Staged xWeatherRouting DLL is missing
+  exit /b 1
+)
+
+rem CMake's Visual Studio generator has historically exposed PACKAGE.vcxproj,
+rem but this is not guaranteed by all CMake/CPack combinations.  Invoke CPack
+rem directly so packaging does not depend on a generated convenience project.
+cpack -G TGZ -C %CONFIGURATION% --config CPackConfig.cmake
+if errorlevel 1 exit /b %errorlevel%
+
+for /f %%C in ('dir /b /a:-d xweather_routing_pi-*.tar.gz 2^>nul ^| find /c /v ""') do set "ARCHIVE_COUNT=%%C"
+if not "%ARCHIVE_COUNT%"=="1" (
+  echo Expected exactly one xWeatherRouting archive, found %ARCHIVE_COUNT%
+  exit /b 1
+)
+for /f %%C in ('dir /b /a:-d xweather_routing_pi-*.xml 2^>nul ^| find /c /v ""') do set "METADATA_COUNT=%%C"
+if not "%METADATA_COUNT%"=="1" (
+  echo Expected exactly one xWeatherRouting metadata file, found %METADATA_COUNT%
+  exit /b 1
+)
+for %%F in (xweather_routing_pi-*.tar.gz) do tar -tzf "%%F" > package-contents.txt
+if errorlevel 1 exit /b %errorlevel%
+findstr /i /c:"plugins/xweather_routing_pi.dll" package-contents.txt >nul
+if errorlevel 1 (
+  echo Package does not contain the xWeatherRouting DLL
+  exit /b 1
+)
+findstr /i /c:"plugins/weather_routing_pi.dll" package-contents.txt >nul
+if not errorlevel 1 (
+  echo Package contains the legacy Weather Routing DLL identity
+  exit /b 1
+)
+
+endlocal
